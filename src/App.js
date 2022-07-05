5be97681615884f74c38d5bcee331c052074f6df
Trevor Ogina <82540204+Trevor-crypto243@users.noreply.github.com>
2022-07-06 01:18:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import './App.css';
 import Form from './form'
+import Form2 from './form2'
+
 
 //use two components to fetch data from user and do the api call,then map that component with the api result 
 //in the response section
@@ -15,17 +17,39 @@ class App extends Component {
   }
 
 
-  render() {     
+  componentDidMount(){
+    //fetch('https://jsonplaceholder.typicode.com/users')    
+    var x = 67.9;
+    var y = 56.8;
+    // fetch("https://api.open-meteo.com/v1/forecast?latitude="+x+"&longitude="+y+"&hourly=temperature_2m")
+    fetch("https://api.open-meteo.com/v1/forecast?latitude="+x+"&longitude="+y+"&hourly=temperature_2m,relativehumidity_2m,cloudcover_mid,windspeed_120m")    
+    .then(res=>res.json())
+    .then(json=>{
+      this.setState({
+        isloaded:true,      
+        items : json, 
+        })
+    });
+  }
+
+
+  render() {    
+
+    var {isloaded,items}=this.state;   
+    if(!isloaded){
+      return <div>Loading...</div>
+    }else{
         return ( 
         <div className="App">
           <div className="App">
             <h1>HOURLY WEATHER FORECAST APP</h1>
             <hr />
+            <h4>Enter the lattitude and the longitude values below...</h4>
             <div className="request"> 
             <Form />       
             </div> 
             <div className="response">
-            <div class="show">          
+            <div className="show">          
            
                 <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div className="modal-dialog">
@@ -36,12 +60,18 @@ class App extends Component {
                         </div>
                         <div className="modal-body">
                           {/* The response compnent goes here<Response /> */} 
-                          <p>Details from the api should be rendered here</p>                               
+                          <p>The following are the weather updates</p>                               
+                          <ul>
+                            <li>Temprature : {items.hourly.temperature_2m.slice(0,5).join(",")}</li>
+                            <li>Wind speed : {items.hourly.windspeed_120m.slice(0,5).join(",f")}</li>
+                            <li>Humidity : {items.hourly.relativehumidity_2m.slice(0,5).join(",")}</li>
+                            <li>Cloud cover : {items.hourly.cloudcover_mid.slice(0,5).join(",")}</li>
+                          </ul>
                         
                         </div>
                         <div className="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>               
-                        </div>
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>               
+                        </div>                        
                     </div>
                     </div>
                 </div>
@@ -49,67 +79,10 @@ class App extends Component {
             </div>
          </div>       
       </div>
-    );
-  //}         
+    );  //}         
     
-  }}
-
+  }}}
 
 export default App;
 
 
-// function App() {  
-//   return (
-//     <><Response />
-//     <div className="App">
-//       <h1>HOURLY WEATHER FORECAST APP</h1>
-//       <hr />
-//       <div className="request">
-//         <form>
-//           <div className="mb-3">
-//             <label for="latitude" className="form-label">lattitude</label>
-//             <input type="number" className="form-control" id="latitude" aria-describedby="emailHelp"></input>
-//           </div>
-
-//           <div className="mb-3">
-//             <label for="longitude" className="form-label">Longitude</label>
-//             <input type="number" className="form-control" id="longitude"></input>
-//           </div>
-
-//           <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-//             Get weather updates
-//           </button>
-//         </form>
-//       </div>
-
-//       <div class="show">
-
-//         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-//           <div class="modal-dialog">
-//             <div class="modal-content">
-//               <div class="modal-header">
-//                 <h5 class="modal-title" id="staticBackdropLabel">Weather updates</h5>
-//                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-//               </div>
-//               <div class="modal-body">
-
-//                 <div class="response">
-//                   <div class="temp">The temprature is {items.hourly.temperature_2m}</div>
-//                   <div class="wind">Wind details {items.hourly.windspeed_120m}</div>
-//                   <div class="cloud_cover">Cloud cover {items.hourly.relativehumidity_2m}</div>
-//                   <div class="humidity">The humidity is {items.hourly.cloudcover_mid}</div>
-//                 </div>
-
-//               </div>
-//               <div class="modal-footer">
-//                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div></>
-//   );
-//   }
-
-// export default App;
